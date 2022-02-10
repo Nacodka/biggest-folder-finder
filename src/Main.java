9a8150e31a028d81ce25418a3814edabe16ccbaa
Natali_1510 <nicolaevanat@yandex.ru>
2022-02-10 07:51:06 +0300

    var 1 getFolderSize

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String foldersPath = "C:\\Users\\Natali\\Documents\\Java";
+        File file = new File(foldersPath);
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files
+        ) {
+            sum += getFolderSize(file);
+        }
+        return sum;
+    }
+}
+
